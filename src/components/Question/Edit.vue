92221bbb89933e3a70153cc044ff94ed7a1a200a
Steven Lei <stevenplus@gmail.com>
2022-09-16 11:21:21 +0800

    implement update logic

--- src/components/Question/Edit.vue
+++ src/components/Question/Edit.vue
@@ -1,5 +1,55 @@
 <script>
-export default {};
+import { db } from "../../../firebase-init";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+
+export default {
+  data() {
+    return {
+      question: "",
+      answers: "",
+    };
+  },
+  methods: {
+    async fetchQuestion() {
+      const id = this.$route.params.id;
+
+      const docRef = doc(db, "questions", id);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        this.question = docSnap.data().question;
+        this.answers = docSnap
+          .data()
+          .answers.map((answer) => answer.text)
+          .join("\n");
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    },
+    async updateQuestion() {
+      const id = this.$route.params.id;
+      const answers = this.answers.split("\n").map((answer, index) => {
+        return {
+          text: answer,
+          isCorrect: index === 0,
+        };
+      });
+
+      const docRef = doc(db, "questions", id);
+      await updateDoc(docRef, {
+        question: this.question,
+        answers: answers,
+      });
+      console.log("Document updated with ID: ", docRef.id);
+
+      this.$router.push("/questions");
+    },
+  },
+  async mounted() {
+    await this.fetchQuestion();
+  },
+};
 </script>
 
 <template>
@@ -13,6 +63,7 @@ export default {};
       <input
         type="text"
         id="question"
+        v-model="question"
         class="border rounded px-2 py-1 block flex-1"
         placeholder="Question"
       />
@@ -21,6 +72,7 @@ export default {};
       <label for="answers" class="w-32 text-slate-500">Answers</label>
       <textarea
         id="answers"
+        v-model="answers"
         type="text"
         class="border rounded px-2 py-1 block flex-1 h-40"
         placeholder="Answers line by line, first line is the correct answer"
@@ -38,6 +90,7 @@ export default {};
           py-1
           text-lg
         "
+        @click="updateQuestion"
       >
         Update
       </button>
